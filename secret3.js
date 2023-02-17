c6040e2677142db859f8348ad148b01730a746d5
gitjez <jezgit@gmail.com>
2023-02-17 11:47:27 +0100

    Triple identical secret

---
+++ secret3.js
@@ -0,0 +1,15 @@
+// Generic password 1
+my_password = "MyPa@ss";
+
+var test = 42;
+
+// Generic password 2
+my_password = "MyPa@ss";
+
+var toto = 5;
+
+// Generic password 3
+my_password = "MyPa@ss";
+
+// A comment
+
