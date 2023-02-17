eb14af7cc87c8132f0ce2404cb3076918209c3cf
gitjez <jezgit@gmail.com>
2023-02-17 11:07:30 +0100

    code.js: add a pqsql secret

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
 // Oops! A secret!
 database = aws_lib.connect("AKIAF4CAFJZR45SB1CC1", "ggshnk6ex5u32144AWS456/JKGjhz595d89sjsny")
 
+// and another one!
+var pg_port=1234;
+var pg_host="mysuperhost.com:9032/MYDB";
+var pg_user="root";
+var pg_pass="Sup3rP@ss";
+
+var mongo_uri = "mongodb+srv://admin:tralala@hohohost.mongodb.net/test?retryWrites=true&w=majority"
+
