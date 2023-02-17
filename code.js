269011b643f77b6ce8f2af2bc75a9960074ff7cd
gitjez <jezgit@gmail.com>
2023-02-17 11:17:22 +0100

    gen password

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 // Oops! A secret!
-database = aws_lib.connect("AKIAF4CAFJZR45SB1CC1", "ggshnk6ex5u32144AWS456/JKGjhz595d89sjsny")
+database = aws_lib.connect("AKIAF4CAFJZR45SB1CC1", "ggshnk6ex5u32144AWS456/JKGjhz595d89sjsny");
 
 // and another one!
 var pg_port=4321;
@@ -7,5 +7,7 @@ var pg_host="mysuperhohohost.com:9033/MYDB";
 var pg_user="root";
 var pg_pass="Sup3rP@ssw0rd";
 
-var mongo_uri = "mongodb+srv://admin:trololo@hahahast.mongodb.net/test?retryWrites=true&w=majority"
+var mongo_uri = "mongodb+srv://admin:trololo@hahahast.mongodb.net/test?retryWrites=true&w=majority";
 
+// Generic password
+my_password = "Sup3rS3cr3t";
