184ab3f6a97455c8669046306ca92dc03fe65362
gitjez <jezgit@gmail.com>
2023-02-17 11:18:55 +0100

    Super generic password added to code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 // Oops! A secret!
-database = aws_lib.connect("AKIAF4CAFJZR45SB1CC1", "ggshnk6ex5u32144AWS456/JKGjhz595d89sjsny")
+database = aws_lib.connect("AKIAF4CAFJZR45SB1CC1", "ggshnk6ex5u32144AWS456/JKGjhz595d89sjsny");
 
 // and another one!
 var pg_port=1234;
@@ -7,5 +7,8 @@ var pg_host="mysuperhost.com:9032/MYDB";
 var pg_user="root";
 var pg_pass="Sup3rP@ss";
 
-var mongo_uri = "mongodb+srv://admin:tralala@hohohost.mongodb.net/test?retryWrites=true&w=majority"
+var mongo_uri = "mongodb+srv://admin:tralala@hohohost.mongodb.net/test?retryWrites=true&w=majority";
+
+// Generic password
+my_password = "Sup3rS3cr3tPa@ssw0rd";
 
