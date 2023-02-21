23c82d54cea6128ee3a3d31065cc37378d46369f
gitjez <jezgit@gmail.com>
2023-02-21 15:02:13 +0100

    new secret in code & secrets

--- code.js
+++ code.js
@@ -12,3 +12,6 @@ var mongo_uri = "mongodb+srv://admin:tralala@hohohost.mongodb.net/test?retryWrit
 // Generic password
 my_password = "Sup3rS3cr3tPa@ssw0rd";
 
+// Again
+mypwd = "Th1s1sSup3rS3cr3t";
+
--- secrets.js
+++ secrets.js
@@ -4,3 +4,7 @@ my_password = "Sup3rS3cr3tPa@ssw0rd";
 // Generic password
 my_password = "Sup3rS3cr3tPa@ssw0rd";
 
+
+// Again
+mypwd = "Th1s1sSup3rS3cr3t";
+
