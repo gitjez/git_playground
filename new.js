d0841af1cdac55a2cb1f3400068bb5ea7c82581e
gitjez <jezgit@gmail.com>
2023-03-07 14:29:06 +0100

    clean

--- new.js
+++ new.js
@@ -1,11 +1,2 @@
 // OK
-// new comment
-// hello
 
-monpass = "Passw0rdS3cr3t";
-
-monpass = "KJZHDI38H6HUDI6GD3643Y5D4F3";
-
-passfromarge = "KHKKDH6EH5D76E54DG7635G7D653G753DG";
-
-passfrommarge = "HE7I6H3876HD3875G3856835D73535";
