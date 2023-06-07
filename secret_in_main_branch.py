fb231f36f9f3f23a9ed6adc03be313c18fb20d7c
gitjez <jezgit@gmail.com>
2023-06-07 16:46:51 +0200

    new database secret in main branch 2

--- secret_in_main_branch.py
+++ secret_in_main_branch.py
@@ -1,5 +1,5 @@
 
 
 // Secret in main branch
-database = aws_lib.connect("AKIAF4L0LJZR45SB1HZ5", "ggshnl2ex5u32188AWS456/JKGjhz595d89sjdpg");
+database = aws_lib.connect("AKIAF4CAFJZR45SB1CC1", "ggshnk6ex5u32144AWS456/JKGjhz595d89sjsny")
 
