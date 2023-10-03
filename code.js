5337060a5165534933dbcacaee812cb2b3f419a4
gitjez <jezgit@gmail.com>
2023-10-03 14:46:38 +0200

    secret leaked

--- code.js
+++ code.js
@@ -17,3 +17,7 @@ password = "ZJH2276DB375EB38W2T";
 
 slack_key = "xoxb-163213206324-FGqsdMF8t18v6N7Oq4i99vj1"
 
+slack_key = "xoxb-163213206324-FGqsdMF8t18v6N7Oq4i99vj2"
+slack_key = "xoxb-163213206324-FGqsdMF8t18v6N7Oq4i99vj3"
+slack_key = "xoxb-163213206324-FGqsdMF8t18v6N7Oq4i99vj4"
+slack_key = "xoxb-163213206324-FGqsdMF8t18v6N7Oq4i99vj5"
